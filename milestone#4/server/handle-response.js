c37c7e1ce904bb722a3780bcb905294bf6a972e6
SOWMITHA T <159044928+SOWMITHA36@users.noreply.github.com>
2024-07-24 11:59:09 +0530

    Delete milestone#4 directory

--- milestone#4/server/handle-response.js
+++
@@ -1,9 +0,0 @@
-exports = {
-  handleFreshdeskResponse: function(data) {
-    console.log("Freshdesk Response:", data);
-  },
-  
-  handleError: function(error) {
-    console.error("Error:", error);
-  }
-};
