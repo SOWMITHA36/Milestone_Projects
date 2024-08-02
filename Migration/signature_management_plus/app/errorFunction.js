8e284b58be1b09d87b00f930e33a57f7b4756dc6
SOWMITHA T <159044928+SOWMITHA36@users.noreply.github.com>
2024-08-02 14:44:16 +0530

    Delete Migration/signature_management_plus directory

--- Migration/signature_management_plus/app/errorFunction.js
+++
@@ -1,7 +0,0 @@
-function errorFunction(client, err){
-    var msg = 'Error. Please try refreshing the page or contact support.';
-    if(client && client.interface && client.interface.trigger){
-        client.interface.trigger("showNotify", {type: "danger", message: msg});
-    }
-    console.error(err);
-}
