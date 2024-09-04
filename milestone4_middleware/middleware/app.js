5a3cc752d3575d6a88a324e2ffc5bf77a4e7cca8
SOWMITHA T <159044928+SOWMITHA36@users.noreply.github.com>
2024-09-04 23:39:43 +0530

    Update

--- milestone4_middleware/middleware/app.js
+++ milestone4_middleware/middleware/app.js
@@ -3,7 +3,8 @@ const http = require("http");
 const cors = require("cors");
 const { port, env } = require("./src/config/env");
 const router = require("./src/routes/index");
-
+const cron = require("cron");
+const configs = require("./src/services/sync.service");
 
 const app = express();
 app.use(express.json());
@@ -18,14 +19,22 @@ app.set("port", port);
 
 app.use("/", router);
 
-app.use('*', function(req, res) {
-    res.status(404).send("Hello, Page not found!");
+app.use('*', function (req, res) {
+	res.status(404).send("Hello, Page not found!");
 });
+function initialize() {
+	try {
 
+		new cron.CronJob('*/2 * * * *', configs.postConfig, null, true, 'America/New_York');
+
+	} catch (error) {
+		console.error('Error during initialization:', error);
+	}
+}
+initialize();
 const server = http.createServer(app);
 server.listen(app.get("port"));
 server.on("error", (err) => {
-	debug(err);
 	console.log(err.message);
 });
 
