03fc04c6e3fa94550ca3e70652d38daf786eaff5
SOWMITHA T <159044928+SOWMITHA36@users.noreply.github.com>
2024-09-04 23:40:54 +0530

    Update suggestions

--- milestone4_middleware/middleware/src/utils/db.js
+++ milestone4_middleware/middleware/src/utils/db.js
@@ -32,30 +32,55 @@ async function getOrCreateCollection(collectionName) {
 async function getValueFromCollection(collectionName) {
     const client = new MongoClient(mongo_db_uri, { useUnifiedTopology: true });
     try {
-        // Connect to the MongoDB cluster
+        
         await client.connect();
         console.log('Connected to MongoDB');
 
-        // Retrieve the collection
+        
         const db = client.db();
         const collection = db.collection(collectionName);
         console.log(`Retrieved collection "${collectionName}"`);
 
-        // Retrieve the value
+        
         const value = await collection.find({}).toArray();;
         console.log(`Retrieved value: ${JSON.stringify(value)}`);
 
-        // Return the value
+        
         return value;
     } catch (err) {
         console.error(err);
         process.exit(1);
     } finally {
+        await client.close();
+        console.log('Disconnected from MongoDB');
+    }
+}
+async function deleteTicketFromCollection(collectionName, ticket) {
+    const client = new MongoClient(mongo_db_uri, { useUnifiedTopology: true });
+    try {
+        
+        await client.connect();
+        console.log('Connected to MongoDB');
+
+        
+        const db = client.db();
+        const collection = db.collection(collectionName);
+        console.log(`Retrieved collection "${collectionName}"`);
+
+        
+        await collection.deleteOne({ id: ticket.id });
+
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    } finally {
+        await client.close();
         console.log('Disconnected from MongoDB');
     }
 }
 
 module.exports = {
     getOrCreateCollection,
-    getValueFromCollection
+    getValueFromCollection,
+    deleteTicketFromCollection
 };
